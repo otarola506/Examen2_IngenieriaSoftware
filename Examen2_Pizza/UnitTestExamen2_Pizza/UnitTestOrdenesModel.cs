286dbf611d8f3a28b26c34548f6fca515b9ad0c4
Sebastian Otarola <otarolasebas@hotmail.com>
2020-07-08 15:46:53 -0600

    [Historia 3]
    [Módulo ordenes clientes]
    
    [Se realizan pruebas unitarias para validar el precio.]

--- Examen2_Pizza/UnitTestExamen2_Pizza/UnitTestOrdenesModel.cs
+++ Examen2_Pizza/UnitTestExamen2_Pizza/UnitTestOrdenesModel.cs
@@ -8,7 +8,7 @@ namespace UnitTestExamen2_Pizza
     public class UnitTestOrdenesModel
     {
         [TestMethod]
-        public void ProcesarIngredientes()
+        public void ProcesarIngredientesVarios()
         {
             OrdenModel Order = new OrdenModel();
             string [] Ingredientes = Order.ProcesarStringIngredientes("Jamón,Hongos,Jalapeños");
@@ -16,6 +16,23 @@ namespace UnitTestExamen2_Pizza
             CollectionAssert.AreEqual(Esperado, Ingredientes);
         }
 
+        [TestMethod]
+        public void ProcesarIngredientesUno()
+        {
+            OrdenModel Order = new OrdenModel();
+            string[] Ingredientes = Order.ProcesarStringIngredientes("Jamón");
+            string[] Esperado = { "Jamón" };
+            CollectionAssert.AreEqual(Esperado, Ingredientes);
+        }
+
+        [TestMethod]
+        public void ProcesarIngredientesVacio()
+        {
+            OrdenModel Order = new OrdenModel();
+            string[] Ingredientes = Order.ProcesarStringIngredientes("");
+            string[] Esperado = { "" };
+            CollectionAssert.AreEqual(Esperado, Ingredientes);
+        }
         [TestMethod]
         public void CalculoPrecioSinImpuestoCorrectoPequena()
         {
@@ -43,7 +60,7 @@ namespace UnitTestExamen2_Pizza
 
         }
         [TestMethod]
-        public void CalculoPrecioSinImpuestoCorrectoError()
+        public void CalculoPrecioSinImpuestoCorrectoTamañoNoExistente()
         {
             OrdenModel Order = new OrdenModel();
             double precio = Order.CalculoPrecioSinImpuesto(5, "Personal");
@@ -79,6 +96,35 @@ namespace UnitTestExamen2_Pizza
             Assert.AreEqual(6650, Math.Round(PrecioSinImpuesto, 2));
             Assert.AreEqual(864.5, Math.Round(Impuesto, 2));
             Assert.AreEqual(8214.5, Math.Round(PrecioFinal, 2));
+
+        }
+
+        [TestMethod]
+        public void CalculoPrecioTotalOrdenMediana()
+        {
+            OrdenModel Order = new OrdenModel();
+            var TripletaPrecio = Order.CalculoPrecioTotalOrden(4, "Mediana");
+            double PrecioSinImpuesto = TripletaPrecio.Item1;
+            double Impuesto = TripletaPrecio.Item2;
+            double PrecioFinal = TripletaPrecio.Item3;
+
+            Assert.AreEqual(5700, Math.Round(PrecioSinImpuesto, 2));
+            Assert.AreEqual(741, Math.Round(Impuesto, 2));
+            Assert.AreEqual(7141, Math.Round(PrecioFinal, 2));
+
+        }
+        [TestMethod]
+        public void CalculoPrecioTotalOrdenGrande()
+        {
+            OrdenModel Order = new OrdenModel();
+            var TripletaPrecio = Order.CalculoPrecioTotalOrden(2, "Grande");
+            double PrecioSinImpuesto = TripletaPrecio.Item1;
+            double Impuesto = TripletaPrecio.Item2;
+            double PrecioFinal = TripletaPrecio.Item3;
+
+            Assert.AreEqual(6400, Math.Round(PrecioSinImpuesto, 2));
+            Assert.AreEqual(832, Math.Round(Impuesto, 2));
+            Assert.AreEqual(7932, Math.Round(PrecioFinal, 2));
 
         }
         [TestMethod]
@@ -128,9 +174,29 @@ namespace UnitTestExamen2_Pizza
         public void ValidarInputProvinciaVacio()
         {
             OrdenModel Order = new OrdenModel();
-            bool resultado = Order.ValidarInputsVacios("Jamón,Hongos", "", "Escazú", "San Rafael", "");
+            bool resultado = Order.ValidarInputsVacios("Jamón,Hongos", "", "Escazú", "San Rafael", "150 mts de la bomba delta, casa porton verde");
 
             Assert.IsFalse(resultado, "La prueba validando que la provincia este vacío no pasó.");
+
+        }
+
+        [TestMethod]
+        public void ValidarInputCantonVacio()
+        {
+            OrdenModel Order = new OrdenModel();
+            bool resultado = Order.ValidarInputsVacios("Jamón,Hongos", "Heredia", "", "San Rafael", "150 mts de la bomba delta, casa porton verde");
+
+            Assert.IsFalse(resultado, "La prueba validando que el cantón este vacío no pasó.");
+
+        }
+
+        [TestMethod]
+        public void ValidarInputDistritoVacio()
+        {
+            OrdenModel Order = new OrdenModel();
+            bool resultado = Order.ValidarInputsVacios("Jamón,Hongos", "Heredia", "Santo Domingo", "", "150 mts de la bomba delta, casa porton verde");
+
+            Assert.IsFalse(resultado, "La prueba validando que el distrito este vacío no pasó.");
 
         }
 
